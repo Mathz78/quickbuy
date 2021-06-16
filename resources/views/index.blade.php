b6e1c6da8fc3a587b813ad0d29f53a790b5bfa22
Matheus <matheusrocha7802@gmail.com>
2021-06-15 21:30:52 -0300

    Add some content in landing page

---
+++ resources/views/index.blade.php
@@ -0,0 +1,70 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <!-- Bootstrap import -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+
+    <style>
+        .backgroundImage {
+            background-image: url("https://www.tshirtgeek.com.br/wp-content/uploads/2021/06/site-tshirtgeek-colecao-computacao-inverno-PT-1.jpg");
+            height: 100vh;
+            background-repeat: no-repeat;
+            /*background-attachment: fixed;*/
+            margin-left: 17%;
+            border-radius: 20px 20px 20px 20px;
+        }
+
+
+        .changeMe {
+            color: red;
+        }
+
+        #mike {
+            color: blue;
+        }
+    </style>
+
+    <title>QuickBuy - HomePage</title>
+</head>
+
+<body>
+        <!-- width: 99%; margin: auto; border-radius: 20px; -->
+        <div style="background-color: #FDFAF6;">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-9 mt-3">
+                        <a href="#" style="text-decoration:none;"><h3>QuickBuy<br><span style="margin-left: 4%;">Store</span></h3></a>
+                    </div>
+
+                    <div class="col-sm-3 mt-4 float-end">
+                        <div class="menu justify-content-end" id="navbarSupportedContent">
+                            <ul class="nav nav-pills">
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#about">Sobre a loja</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#skills">Contatos</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#contact">Login</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="backgroundImage mt-4">
+
+        </div>
+</body>
+
+</html>
